74b298a055b7987952d0e288394d20dd697a33d3
gail64917 <gail64917@yandex.ru>
2019-05-19 22:31:49 +0300

    upd: printed  y1, y2, z

--- Alexandra/Alexandra/Program.cs
+++ Alexandra/Alexandra/Program.cs
@@ -138,11 +138,12 @@ namespace Alexandra
                 matrixResult = GetMatrixResult(CurrentPlayer1Choice, CurrentPlayer2Choice, L, result);
 
                 Console.WriteLine("\r\n\r\nШаг {0}: игрок1 = {1}, игрок2 = {2}. ", step, CurrentPlayer1Choice, CurrentPlayer2Choice);
+                Console.WriteLine("у1 = {0}, y2 = {1}, z = {2}", y1, y2, z);
                 Console.WriteLine(matrixResult);
             }
             #endregion
 
-            Console.WriteLine("\r\n\r\nИтог Игр: {0};{1}", result[0], result[1]);
+            Console.WriteLine("\r\n\r\nИтог Игр: {0}; {1}", result[0], result[1]);
         }
 
         /// <summary>
